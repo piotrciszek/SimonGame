976e8bc0b10083336043bdc3a3b72ecafbfafc2c
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2021-07-06 18:47:30 +0200

    checking which button is pressed

--- game.js
+++ game.js
@@ -2,6 +2,15 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+
+  console.log(userClickedPattern);
+
+});
+
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
